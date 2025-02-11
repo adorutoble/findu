ef217235c53999e649a847811db96299f05178f7
unknown <shabrinaila10@gmail.com>
2025-02-12 00:06:43 +0700

    ui: making navbar and landing page
    - added navbar and logo
    - added hero section
    - making buttons

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-[#F7F8FF]">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css"  rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <title>FindU</title>
+</head>
+<body class="h-full w-full font-[poppins]">
+<div class="min-h-full">
+  <x-navbar></x-navbar>
+    <main>
+      <div class="px-4 py-6 sm:px-6 lg:px-8">
+        {{ $slot }}
+      </div>
+    </main>
+  </div>
+  <x-footer></x-footer>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<x-layout>
+    {{-- First Hero --}}
+    <section>
+        <div class="grid max-w-screen-xl px-4 py-8 mx-16 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div class="mr-auto place-self-center lg:col-span-7">
+                <h1 class="font-[poppins] w-7xl mb-4 text-5xl font-extrabold tracking-tight leading-none">Platform terdepan temukan universitas Impianmu!</h1>
+                <p class="max-w-2xl text-lg mb-6 font-light text-gray-500 lg:mb-8 dark:text-gray-400">Jelajahi ribuan pilihan universitas, sesuaikan dengan minat dan kebutuhanmu, dan wujudkan masa depan yang gemilang bersama FindU.</p>
+                <a href="#" class="inline-flex items-center justify-center px-10 py-3 mr-3 text-base font-medium text-center text-white rounded-full bg-[#233A75] focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                    Ayo Mulai!
+                </a>
+            </div>
+            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+                <img src="./img/student-hero.png" alt="hero1">
+            </div>                
+        </div>
+    </section>
+
+    {{-- Section 2 --}}
+    <section class="w-screen">
+
+    </section>
+
+</x-layout>
